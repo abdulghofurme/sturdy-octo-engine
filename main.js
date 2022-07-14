f12b29e402f5089b41d92aaba35fb83f2fdcc1f3
Abdul Ghofur <abdulghofur.me@gmail.com>
2022-07-15 06:16:56 +0700

    Respond to a mouseover event in Vue with the v-on directive
    add faker to create random image

--- main.js
+++ main.js
@@ -3,15 +3,35 @@ const app = Vue.createApp({
     return {
       title: "Comedy events",
       tickets: 50,
-      image: "https://picsum.photos/200",
+      image: faker.image.image(200, 200, true),
       details: ["Funny comics", "Mime artistry", "Satire", "Sketches"],
       cart: 0,
+      ticketTypes: [
+        {
+          id: "bronze",
+          name: "Bronze",
+          image: faker.image.image(200, 200, true),
+        },
+        {
+          id: "silver",
+          name: "Silver",
+          image: faker.image.image(200, 200, true),
+        },
+        {
+          id: "gold",
+          name: "Gold",
+          image: faker.image.image(200, 200, true),
+        },
+      ],
     };
   },
   methods: {
     updateCart(number = 1) {
       this.cart += number;
       this.tickets -= number;
+    },
+    updateImage(image) {
+      this.image = image;
     },
   },
 });

fce1403422a6e1715eae6141cb92019c9e19f035
Abdul Ghofur <abdulghofur.me@gmail.com>
2022-07-15 15:34:40 +0700

    Create and Manipulate State within a Vue Component by Defining Methods and Properties

--- components/ticketDetails.js
+++ components/ticketDetails.js
@@ -1,22 +1,31 @@
 app.component("ticket-details", {
   props: {
-    type: {
-      id: String,
-      name: String,
-      image: String,
-      tickets: Number,
+    name: String,
+    image: String,
+    tickets: Number,
+  },
+  data() {
+    return {
+      ticketsToBuy: 0,
+      ticketsLeft: this.tickets,
+    };
+  },
+  methods: {
+    addTickets(num = 1) {
+      this.ticketsLeft -= num;
+      this.ticketsToBuy = 0;
     },
   },
-  data: () => ({
-    ticketToBuy: 0,
-  }),
   template: `
- 	<li>
-  	<div>{{ type.name }}</div>
-		<select v-model='ticketToBuy'>
-			<option value='0'>0</option>
-			<option v-for='number in type.tickets' :value='number'>{{number}}</option>
-		</select>
+  <li>
+  	<div>{{ name }}</div>
+
+  	<select v-model='ticketsToBuy'>
+  		<option value='0'>0</option>
+  		<option v-for='number in ticketsLeft' :value='number'>{{number}}</option>
+  	</select>
+
+  	<button v-if='ticketsToBuy > 0' @click='addTickets(ticketsToBuy)'>Buy</button>
   </li>
-	`,
+  `,
 });
